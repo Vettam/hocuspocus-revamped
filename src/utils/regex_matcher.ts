e7f9306b89591dbad7ff790913ea008e35181050
Delwin Mathew <84124091+opxdelwin@users.noreply.github.com>
2025-10-31 15:43:52 +0530

    fix: separate uuid testers for individual and pair

--- src/utils/regex_matcher.ts
+++ src/utils/regex_matcher.ts
@@ -1,9 +1,17 @@
 class RegexMatcher {
   static uuidRegex: string =
-    "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
+    "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}";
 
+  // Matches a string that is exactly a UUID
   static matchUUID(input: string): boolean {
-    return RegExp(this.uuidRegex).test(input);
+    const re = new RegExp(`^${this.uuidRegex}$`);
+    return re.test(input);
+  }
+
+  // Matches a string that is exactly <uuid>:<uuid>
+  static matchUUIDPair(input: string): boolean {
+    const re = new RegExp(`^${this.uuidRegex}:${this.uuidRegex}$`);
+    return re.test(input);
   }
 }
 
